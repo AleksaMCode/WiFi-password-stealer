8e1f4eb63f2508fc22bb6fdf3f8d160388fff71b
AleksaMCode <aleksamcode@gmail.com>
2022-12-07 20:34:53 +0100

    Wifi password list script added

---
+++ wifi_password_script.ps1
@@ -0,0 +1,9 @@
+﻿$Profiles=@()
+$Profiles +=(netsh wlan show profiles) | Select-String "\:(.+)$" | Foreach{$_.Matches.Groups[1].Value.Trim()}
+$res = $Profiles | Foreach{$SSID = $_ ; (netsh wlan show profile name="$_" key=clear)} |
+            Select-String "Key Content\W+\:(.+)$" |
+                Foreach{$pass=$_.Matches.Groups[1].Value.Trim(); $_} |
+                    Foreach{[PSCustomObject]@{Wireless_Network_Name=$SSID;Password=$pass}}
+                        Format-Table -AutoSize
+			
+Out-File -FilePath .\wifi_pass.txt -InputObject $res -Encoding ASCII -Width 50
