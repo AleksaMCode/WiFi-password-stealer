49b13aa0896e5d641d2f7f295210fab28755a794
AleksaMCode <aleksamcode@gmail.com>
2023-05-11 21:34:20 +0200

    Wifi password list script added

---
+++ scripts/wifi_passwords_print.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo -e "Wireless_Network_Name Password\n--------------------- --------" > ~/Desktop/wifi_pass.txt ; for FILE in *; do echo -e "$(sudo <<< "pass" cat "$FILE" | grep -oP '(?<=ssid=).*') \t\t\t\t  $(sudo <<< "pass" cat "$FILE" | grep -oP '(?<=psk=).*')"; done >> ~/Desktop/wifi_pass.txt;
