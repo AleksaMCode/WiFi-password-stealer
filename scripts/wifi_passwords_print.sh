3ff16ce0e093ef11bb7b47fc1b70383f7a910a34
AleksaMCode <aleksamcode@gmail.com>
2023-05-12 21:05:31 +0200

    Typo bug removed

--- scripts/wifi_passwords_print.sh
+++ scripts/wifi_passwords_print.sh
@@ -4,4 +4,4 @@ echo "Wireless_Network_Name Password\n--------------------- --------" > ~/Deskto
 for FILE in /etc/NetworkManager/system-connections/*
 do
     echo "$(cat "$FILE" | grep -oP '(?<=ssid=).*') \t\t\t\t  $(cat "$FILE" | grep -oP '(?<=psk=).*')"
-done >> media/$(hostname)/UsbStick//wifi_pass.txt
+done >> media/$(hostname)/UsbStick/wifi_pass.txt
