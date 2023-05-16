19844a1ed42a4dcc96e1264acd7163ad9100a717
AleksaMCode <aleksamcode@gmail.com>
2023-05-16 14:52:41 +0200

    File creation location bug removed

--- scripts/wifi_passwords_print.sh
+++ scripts/wifi_passwords_print.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "Wireless_Network_Name Password\n--------------------- --------" > ~/Desktop/wifi_pass.txt
+echo "Wireless_Network_Name Password\n--------------------- --------" > /media/$(hostname)/UsbStick/wifi_pass.txt
 
 for FILE in /etc/NetworkManager/system-connections/*
 do
     echo "$(cat "$FILE" | grep -oP '(?<=ssid=).*') \t\t\t\t  $(cat "$FILE" | grep -oP '(?<=psk=).*')"
-done >> media/$(hostname)/UsbStick/wifi_pass.txt
+done >> /media/$(hostname)/UsbStick/wifi_pass.txt
