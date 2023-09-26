e5eca31f8a6f8b3d0ed1a5ffa26a5c6da9fe2dae
AleksaMCode <aleksamcode@gmail.com>
2023-09-26 11:15:14 +0200

    Update `Bash` script Add argument that contains USB name

--- scripts/wifi_passwords_print.sh
+++ scripts/wifi_passwords_print.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "Wireless_Network_Name Password\n--------------------- --------" > /media/$(hostname)/USBSTICK/wifi_pass.txt
+echo "Wireless_Network_Name Password\n--------------------- --------" > /media/$(hostname)/$1/wifi_pass.txt
 
 for FILE in /etc/NetworkManager/system-connections/*
 do
     echo "$(cat "$FILE" | grep -oP '(?<=ssid=).*') \t\t\t\t  $(cat "$FILE" | grep -oP '(?<=psk=).*')"
-done >> /media/$(hostname)/USBSTICK/wifi_pass.txt
+done >> /media/$(hostname)/$1/wifi_pass.txt
